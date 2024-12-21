2d6078b884e3687f837e2d95b8a5cf08d4815079
BreicheSinal <sinalbreiche@gmail.com>
2024-12-21 17:58:09 +0200

    Added the execution of deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,13 +2,23 @@ import { ethers } from "hardhat";
 
 async function main() {
   // getting contract
-  const TrophyVerification = await ethers.getContractFactory("TrophyVerification");
+  const TrophyVerification = await ethers.getContractFactory(
+    "TrophyVerification"
+  );
 
-   // deploying the contract
-   console.log("Deploying TrophyVerification...");
-   const trophy = await TrophyVerification.deploy();
-   await trophy.waitForDeployment();
- 
-   const address = await trophy.getAddress();
-   console.log("TrophyVerification deployed to:", address);
+  // deploying the contract
+  console.log("Deploying TrophyVerification...");
+  const trophy = await TrophyVerification.deploy();
+  await trophy.waitForDeployment();
+
+  const address = await trophy.getAddress();
+  console.log("TrophyVerification deployed to:", address);
 }
+
+// executing deployment
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
