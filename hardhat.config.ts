cdf4948285eb8e2ae9423489bc2f5384738e1174
BreicheSinal <sinalbreiche@gmail.com>
2024-12-21 17:47:23 +0200

    Updated hardhat.config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    localhost: {
+      url: process.env.LOCALHOST_URL,
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
 
 export default config;
