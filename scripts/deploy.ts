71524147244e30fb5996578350821e9f50153aa1
BreicheSinal <sinalbreiche@gmail.com>
2024-12-21 17:56:32 +0200

    Revert "Deployed contract"
    This reverts commit 7182c901e5f0a7bb2413405c1d6d43a478879cab.

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,13 +2,13 @@ import { ethers } from "hardhat";
 
 async function main() {
   // getting contract
-  const TrophyVerification = await ethers.getContractFactory(
-    "TrophyVerification"
-  );
+  const TrophyVerification = await ethers.getContractFactory("TrophyVerification");
 
-  // deploying the contract
-  console.log("Deploying TrophyVerification...");
-
-  const trophy = await TrophyVerification.deploy();
-  await trophy.waitForDeployment();
+   // deploying the contract
+   console.log("Deploying TrophyVerification...");
+   const trophy = await TrophyVerification.deploy();
+   await trophy.waitForDeployment();
+ 
+   const address = await trophy.getAddress();
+   console.log("TrophyVerification deployed to:", address);
 }
