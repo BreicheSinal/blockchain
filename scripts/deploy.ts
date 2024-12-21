7182c901e5f0a7bb2413405c1d6d43a478879cab
BreicheSinal <sinalbreiche@gmail.com>
2024-12-21 17:53:59 +0200

    Deployed contract

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,5 +2,13 @@ import { ethers } from "hardhat";
 
 async function main() {
   // getting contract
-  const TrophyVerification = await ethers.getContractFactory("TrophyVerification");
+  const TrophyVerification = await ethers.getContractFactory(
+    "TrophyVerification"
+  );
+
+  // deploying the contract
+  console.log("Deploying TrophyVerification...");
+
+  const trophy = await TrophyVerification.deploy();
+  await trophy.waitForDeployment();
 }
